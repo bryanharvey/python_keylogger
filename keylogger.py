1f2b534506583fa6b9442ceec897911689c51bff
Bryan Harvey <bryan.edward.harvey@gmail.com>
2016-05-05 16:46:38 -0700

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from evdev import InputDevice, categorize, ecodes
+dev = InputDevice('/dev/input/event1')
+
+print(dev)
+
+for event in dev.read_loop():
+    if event.type == ecodes.EV_KEY:
+        print(categorize(event))
